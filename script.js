61758bbb855c6792c7602d06a05192fb3edf30c6
Zan Hiroo <95255730+HIROS-DEV@users.noreply.github.com>
2022-10-24 11:48:06 +0900

    add Pseudocode in script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// create string variables playerSelection
+// create string variables computerSelection
+
+// ask the user "playerSelection: Rock or Paper or Scissors" and put answer in playerSelection
+// If user's input do not equal "Rock" or "Paper" or "Scissors", this is error. So, show prompt message again.
+// playerSelection must be case-incentive.
+
+// put the computer's answer in computerSelection automatically.
+// In this project, we use getComputerChoice function. So create it!!
+
+// compare with playerSelection and computerSelection.
+// In this project, we use playRound function(playRound function takes two parameters(playerSelection, computerSelection)).
+// So create it!!
+
+// This is not a single round game. Multiple rounds game. We should create round function for multiple games.
+// Create game function and call the playRound function inside it. game function should use loops (for or while)
+// must be keep score(s) and report winner (create scores variable, and show message)
+
+// If game ends, once more function and reset previous all data.

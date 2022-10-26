e243156c167d212e5096dbf202484f9af3b23261
Zan Hiroo <95255730+HIROS-DEV@users.noreply.github.com>
2022-10-26 17:19:07 +0900

    create game fundamentals in script.js

---
+++ Pseudocode.md
@@ -0,0 +1,29 @@
+Pseudocode(Project Overview) - This is a simple Pseudocode for this project
+
+1.Rock Paper Scissors
+
+create string variables playerSelection
+create string variables computerSelection
+ask the user "playerSelection: Rock or Paper or Scissors" and put answer in playerSelection
+
+If user's input do not equal "Rock" or "Paper" or "Scissors", this is error. So, show prompt message again.
+
+playerSelection must be case-incentive.
+put the computer's answer in computerSelection automatically.
+
+In this project, we use getComputerChoice function. So create it!!
+compare with playerSelection and computerSelection.
+In this project, we use playRound function(playRound function takes two parameters(playerSelection, computerSelection)).
+So create it!!
+This is not a single round game. Multiple rounds game. We should create round function for multiple games.
+Create game function and call the playRound function inside it. game function should use loops (for or while)
+must be keep score(s) and report winner (create scores variable, and show message)
+If game ends, once more function and reset previous all data.
+
+
+2.Revisit Rock Paper Scissors
+TODO: 1. remove the logic that plays exactly five rounds
+TODO: 2. create three buttons. Add playRound function
+TODO: 3. Add for displaying results
+TODO: 4. Display the score and final announce winner
+TODO: 5. Play again button, and reset function
--- script.js
+++ script.js
@@ -1,42 +1,37 @@
-// create string variables playerSelection
-// create string variables computerSelection
-
-// ask the user "playerSelection: Rock or Paper or Scissors" and put answer in playerSelection
-// If user's input do not equal "Rock" or "Paper" or "Scissors", this is error. So, show prompt message again.
-// playerSelection must be case-incentive.
-// put the computer's answer in computerSelection automatically.
-// In this project, we use getComputerChoice function. So create it!!
-
-// compare with playerSelection and computerSelection.
-// In this project, we use playRound function(playRound function takes two parameters(playerSelection, computerSelection)).
-// So create it!!
-
-// This is not a single round game. Multiple rounds game. We should create round function for multiple games.
-// Create game function and call the playRound function inside it. game function should use loops (for or while)
-// must be keep score(s) and report winner (create scores variable, and show message)
-
-// If game ends, once more function and reset previous all data.
-
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection;
+let computerSelection;
 
-function getPlayerSelection() {
-	const playerChoice = prompt('Rock! Paper! Scissors!');
-	if (!playerChoice) return;
+const container = document.querySelector('.container');
+const startBtn = document.querySelector('#start');
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const resetBtn = document.querySelector('#reset');
 
-	const playerChoiceCaseInsensitive =
-		playerChoice[0].toUpperCase() + playerChoice.slice(1).toLowerCase();
+let resultText = document.createElement('p');
 
-	if (
-		playerChoiceCaseInsensitive === 'Rock' ||
-		playerChoiceCaseInsensitive === 'Paper' ||
-		playerChoiceCaseInsensitive === 'Scissors'
-	) {
-		return playerChoiceCaseInsensitive;
-	} else {
-		alert('Please input "Rock" or "Paper" or "Scissors". \n Case Insensitive.');
-		return getPlayerSelection();
-	}
+function startGame() {
+	startBtn.classList.add('invisible');
+	rockBtn.classList.remove('invisible');
+	paperBtn.classList.remove('invisible');
+	scissorsBtn.classList.remove('invisible');
+	resetBtn.classList.remove('invisible');
+	resultText.innerText = '';
+}
+
+function resetGame() {
+	startBtn.classList.remove('invisible');
+	rockBtn.classList.add('invisible');
+	paperBtn.classList.add('invisible');
+	scissorsBtn.classList.add('invisible');
+	resetBtn.classList.add('invisible');
+
+	playerScore = 0;
+	computerScore = 0;
+	playerSelection = '';
+	computerSelection = '';
 }
 
 function getComputerChoice() {
@@ -46,84 +41,83 @@ function getComputerChoice() {
 	return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-	console.log(playerSelection, computerSelection);
+function printResultForWin() {
+	playerScore += 1;
+	resultText.innerText = `You Win! ${playerSelection} wins ${computerSelection}`;
+	container.appendChild(resultText);
+}
+
+function printResultForLose() {
+	computerScore += 1;
+	resultText.innerText = `You Lose! ${playerSelection} beats ${computerSelection}`;
+	container.appendChild(resultText);
+}
+
+function printResultForEven() {
+	playerScore += 0;
+	computerScore += 0;
+	resultText.innerText = `Even! ${playerSelection} evens ${computerSelection}`;
+	container.appendChild(resultText);
+}
+
+function printFinalResultForWin() {
+	resultText.innerText = `Final Result: You Win! Your score is ${playerScore}. Computer score is ${computerScore}`;
+	container.appendChild(resultText);
+	resetGame();
+}
+
+function printFinalResultForLose() {
+	resultText.innerText = `Final Result: You Lose! Your score is ${playerScore}. Computer score is ${computerScore}`;
+	container.appendChild(resultText);
+	resetGame();
+}
+
+function playRound(event) {
+	playerSelection = event.target.innerText;
+	computerSelection = getComputerChoice();
 
 	if (playerSelection === computerSelection) {
-		playerScore += 0;
-		computerScore += 0;
-		return alert(`Even! ${playerSelection} evens ${computerSelection}`);
+		printResultForEven();
 	}
 
 	switch (playerSelection) {
 		case 'Rock':
 			if (computerSelection === 'Paper') {
-				computerScore += 1;
-				alert(`You Lose! ${playerSelection} beats ${computerSelection}`);
+				printResultForLose();
 			} else if (computerSelection === 'Scissors') {
-				playerScore += 1;
-				alert(`You Win! ${playerSelection} wins ${computerSelection}`);
+				printResultForWin();
 			}
+
 			break;
 		case 'Paper':
 			if (computerSelection === 'Scissors') {
-				computerScore += 1;
-				alert(`You Lose! ${playerSelection} beats ${computerSelection}`);
+				printResultForLose();
 			} else if (computerSelection === 'Rock') {
-				playerScore += 1;
-				alert(`You Win! ${playerSelection} wins ${computerSelection}`);
+				printResultForWin();
 			}
+
 			break;
 		case 'Scissors':
 			if (computerSelection === 'Rock') {
-				computerScore += 1;
-				alert(`You Lose! ${playerSelection} beats ${computerSelection}`);
+				printResultForLose();
 			} else if (computerSelection === 'Paper') {
-				playerScore += 1;
-				alert(`You Win! ${playerSelection} wins ${computerSelection}`);
+				printResultForWin();
 			}
 			break;
 		default:
 			break;
 	}
-}
 
-function game() {
-	for (let i = 1; i <= 5; i++) {
-		const playerSelection = getPlayerSelection();
-		const computerSelection = getComputerChoice();
-
-		if (!playerSelection) {
-			return alert(`Pushed cancel button. Stop the game.`);
-		}
-
-		playRound(playerSelection, computerSelection);
-	}
-
-	if (playerScore > computerScore) {
-		alert(
-			`Final Result: You Win! Your score is ${playerScore}. Computer score is ${computerScore}`
-		);
-	} else if (playerScore === computerScore) {
-		alert(
-			`Final Result: Draw! Your score is ${playerScore}. Computer score is ${computerScore}`
-		);
-	} else {
-		alert(
-			`Final Result: You Lose! Your score is ${playerScore}. Computer score is ${computerScore}`
-		);
-	}
-
-	const retry = prompt('Do you want to play game again?', 'one more!!');
-	if (retry === 'one more!!') {
-		playerScore = 0;
-		computerScore = 0;
-		game();
-	} else {
-		playerScore = 0;
-		computerScore = 0;
-		return;
+	if (playerScore >= 5) {
+		printFinalResultForWin();
+	} else if (computerScore >= 5) {
+		printFinalResultForLose();
 	}
 }
 
-game();
+startBtn.addEventListener('click', startGame);
+resetBtn.addEventListener('click', resetGame);
+
+rockBtn.addEventListener('click', playRound);
+paperBtn.addEventListener('click', playRound);
+scissorsBtn.addEventListener('click', playRound);
